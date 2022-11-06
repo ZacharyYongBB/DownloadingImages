1e2dc81497495a2ba950f8d0991e029f4c5d1456
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-11-06 14:20:51 +0800

    Initial Commit

---
+++ DownloadingImages/DownloadingImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadingImagesApp.swift
+//  DownloadingImages
+//
+//  Created by Zachary on 6/11/22.
+//
+
+import SwiftUI
+
+@main
+struct DownloadingImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DownloadingImages/Views/DownloadingImagesExample.swift
@@ -0,0 +1,20 @@
+//
+//  DownloadingImagesExample.swift
+//  DownloadingImages
+//
+//  Created by Zachary on 6/11/22.
+//
+
+import SwiftUI
+
+struct DownloadingImagesExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DownloadingImagesExample_Previews: PreviewProvider {
+    static var previews: some View {
+        DownloadingImagesExample()
+    }
+}
